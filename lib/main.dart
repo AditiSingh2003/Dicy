1d2bf6c5a5674e83131b5493cf22ae23148e1657
Aditi <adithakurti@gmail.com>
2023-02-01 16:26:42 +0530

    added image and aligned the it

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,53 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 4;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+  return Center(
+    child: Row(
+    children: <Widget>[
+    Expanded(
+      child:  Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextButton(
+      onPressed: () {
+        setState(() {
+          leftDiceNumber = 5;
+          print('object= $leftDiceNumber');
+        });
+            },
+    child: Image.asset('images/dice$leftDiceNumber.png'),
+      ),
+      ),
+    ),
+    Expanded(
+      child:  Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextButton(
+          onPressed: () {
+            print('Right button got pressed. ');
+            },
+          child: Image.asset('images/dice1.png'),
+          ),
+        ),
+      ),
+      ],
+    ),
+    );
+   }
   }
-}
+
+
